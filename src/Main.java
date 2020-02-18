9098c5cd63cdc6e3b90ad87fbb2964afb442d5f2
HASAN <hasanuzzaman.dev@gmail.com>
2020-02-18 15:03:48 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number for factorial");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("Hello World!");
+    }
+}
