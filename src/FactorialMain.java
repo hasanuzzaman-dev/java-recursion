9836cbc32a583a58bb29dbd966388567debf8a68
HASAN <hasanuzzaman.dev@gmail.com>
2020-02-18 15:51:26 +0600

    factorial commit

---
+++ src/FactorialMain.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FactorialMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number for factorial");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        int fact = factorial(num);
+        System.out.println("Factorial: "+fact);
+    }
+
+    private static int factorial(int num){
+        if (num == 0)
+            return 1;
+        else
+            return num * factorial(num - 1);
+
+    }
+}
